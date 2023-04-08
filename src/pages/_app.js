f71fabfaa8e901ccff93d8b8469a5106805f2d40
iamhouser <e.domnin@yahoo.com>
2023-04-08 17:01:27 +0300

    bug fix for render

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,18 +3,15 @@
 // 1. import `NextUIProvider` component
 import { NextUIProvider } from '@nextui-org/react';
 import '../styles/Home.module.css';
-import { useSSR } from '@nextui-org/react'
-import { SSRProvider } from '@react-aria/ssr';
+
 
 function MyApp({ Component, pageProps }) {
 
   // 2. Use at the root of your app
   return  (
-    <SSRProvider>
-    {  <NextUIProvider>
-        <Component {...pageProps} />
-      </NextUIProvider>}
-    </SSRProvider>
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
   )
 }
 
