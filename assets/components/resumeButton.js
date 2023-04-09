b79648267b2da8f5002f7c5d51c8104b138fc5c2
iamhouser <e.domnin@yahoo.com>
2023-04-09 15:08:06 +0300

    minor fix

--- assets/components/resumeButton.js
+++ assets/components/resumeButton.js
@@ -3,13 +3,14 @@ import { Button } from "@nextui-org/react";
 
 
 export const downloadFile = async (fileName) => {
-    const fileUrl = `/public/${fileName}`
+    const fileUrl = `public/${fileName}`
     const response = await fetch(fileUrl);
     const blob = await response.blob()
 
     const link = document.createElement('a');
     link.href = window.URL.createObjectURL(blob);
     link.download = fileName;
+    link.setAttribute('download', fileName);
     link.click();
     window.URL.revokeObjectURL(link.href);
 }
@@ -18,7 +19,7 @@ export const downloadFile = async (fileName) => {
 
 export default function ResumeButton () {
     const handleDownload = () => {
-        downloadFile('Domnin.pdf');
+        downloadFile('Evgeny_Domnin.pdf');
     }
     return (
         <Button size={"sm"} onClick={handleDownload} shadow auto >Download .pdf</Button>
